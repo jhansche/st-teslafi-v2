9fe0e1e4552b485126633012a97c34d6a04d3dad
Joe Hansche <madcoder@gmail.com>
2021-02-15 21:44:00 -0500

    Moving v2 DTHs to new project

---
+++ devicetypes/chapterdream03931/tesla-climate.src/tesla-climate.groovy
@@ -0,0 +1,91 @@
+/**
+ *  Tesla
+ *
+ *  Copyright 2019 Joe Hansche
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+    definition(
+            name: "Tesla (Climate)",
+            namespace: "chapterdream03931",
+            author: "Joe Hansche",
+            mnmn: "SmartThingsCommunity",
+            // ocfDeviceType: "x.com.st.d.tesla",
+            ocfDeviceType: "oic.d.vehicleconnector",
+            vid: "b337fdb0-9908-3d2a-96c9-aac49920eb27"
+    ) {
+        capability "Temperature Measurement"
+        capability "Thermostat Mode" // .thermostatMode = [auto, off]
+        capability "Thermostat Setpoint"
+
+        // FIXME: Why doesn't Thermostat Setpoint define this?
+        command "setThermostatSetpoint"
+    }
+}
+
+def initialize() {
+    log.debug "Executing 'initialize'"
+    log.debug "Parent (main) = ${parent}"
+    log.debug "Parent (app) = ${parent.parent}"
+    sendEvent(name: "supportedThermostatModes", value: ["auto", "off"])
+}
+
+def processData(data) {
+    log.debug "processData for Climate: ${data}"
+    if (!data) {
+        log.error "No data found for ${device.deviceNetworkId}"
+        return
+    }
+
+    if (data.climateState) {
+        sendEvent(name: "temperature", value: data.climateState.temperature, unit: 'F')
+        sendEvent(name: "thermostatSetpoint", value: data.climateState.thermostatSetpoint, unit: 'F')
+        sendEvent(name: "thermostatMode", value: data.climateState.thermostatMode)
+    }
+}
+
+def auto() {
+    log.debug "Executing 'thermostatMode.auto'"
+    def result = parent.climateAuto(this)
+    if (result) doRefresh()
+}
+
+def off() {
+    log.debug "Executing 'thermostatMode.off'"
+    def result = parent.climateOff(this)
+    if (result) doRefresh()
+}
+
+def heat() { log.info "Executing 'thermostatMode.heat' - Not supported" }
+
+def emergencyHeat() { log.info "Executing 'thermostatMode.emergencyHeat' - Not supported" }
+
+def cool() { log.info "Executing 'thermostatMode.cool' - Not supported" }
+
+def setThermostatMode(mode) {
+    log.debug "Executing 'setThermostatMode'"
+    if (mode == "auto") {
+        auto()
+    } else if (mode == "off") {
+        off()
+    } else {
+        log.error "setThermostatMode: Only thermostat modes Auto and Off are supported"
+    }
+}
+
+// FIXME: Thermostat.thermostatSetpoint is deprecated, replaced with heatingSetpoint / coolingSetpoint.
+//  thermostatSetpoint capability does not have a setThermostatSetpoint defined.
+def setThermostatSetpoint(setpoint) {
+    log.debug "Executing 'thermostat.setThermostatSetpoint'"
+    def result = parent.setThermostatSetpoint(this, setpoint)
+    if (result) doRefresh()
+}
---
+++ devicetypes/chapterdream03931/tesla-main.src/tesla-main.groovy
@@ -0,0 +1,151 @@
+/**
+ *  Tesla
+ *
+ *  Copyright 2019 Joe Hansche
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+    definition(
+            name: "Tesla (Main)",
+            namespace: "chapterdream03931",
+            author: "Joe Hansche",
+            mnmn: "SmartThingsCommunity",
+            // ocfDeviceType: "x.com.st.d.tesla",
+            ocfDeviceType: "oic.d.vehicleconnector",
+            vid: "ac7730d0-39cb-341c-9ada-cc56fd04efc0"
+    ) {
+        capability "Refresh" // refresh()
+
+		capability "chapterdream03931.softwareVersion"
+        capability "chapterdream03931.softwareUpdate" // checkForUpdate()
+
+        capability "Temperature Measurement" // for OUTSIDE temp only
+    }
+}
+
+def initialize() {
+    log.debug "Executing 'initialize' with device: ${device.deviceNetworkId}"
+    ensureComponentDevices()
+
+    runIn(2, doRefresh)
+    runEvery5Minutes(doRefresh)
+}
+
+private ensureComponentDevices() {
+	log.debug("All child devices (before): ${getChildDevices()}")
+
+	def dni = device.deviceNetworkId
+    def children = getChildDevices()
+    def d
+    
+    d = children.find { it.deviceNetworkid == "${dni}:car" }
+    if (d) {
+    	log.debug ":car device exists: $d"
+    } else {
+    	// create :car
+        addChildDevice("chapterdream03931", "Tesla (Car)", "${dni}:car", null, [completedSetup: true, label: "${device.displayName} (car)", isComponent: true, componentName: "car", componentLabel: "Car Data"])
+    }
+    
+    d = children.find { it.deviceNetworkid == "${dni}:climate" }
+    if (d) {
+    	log.debug ":climate device exists: $d"
+    } else {
+        addChildDevice("chapterdream03931", "Tesla (Climate)", "${dni}:climate", null, [completedSetup: true, label: "${device.displayName} (climate)", isComponent: true, componentName: "climate", componentLabel: "Climate Data"])
+    }
+
+	d = children.find { it.deviceNetworkid == "${dni}:battery" }
+    if (d) {
+    	log.debug ":battery device exists: $d"
+    } else {
+        addChildDevice("chapterdream03931", "Tesla (Battery)", "${dni}:battery", null, [completedSetup: true, label: "${device.displayName} (battery)", isComponent: true, componentName: "battery", componentLabel: "Battery Data"])
+    }
+
+	d = children.find { it.deviceNetworkid == "${dni}:charger" }
+    if (d) {
+    	log.debug ":charger device exists: $d"
+    } else {
+        addChildDevice("chapterdream03931", "Tesla (Charger)", "${dni}:charger", null, [completedSetup: true, label: "${device.displayName} (charger)", isComponent: true, componentName: "charger", componentLabel: "Charging Data"])
+    }
+
+	childDevices.each { it.initialize() }
+	log.debug("All child devices (after): ${getChildDevices()}")
+}
+
+private processData(data) {
+    log.debug "processData: ${data}"
+    if (!data) {
+        log.error "No data found for ${device.deviceNetworkId}"
+        return
+    }
+
+    sendEvent(name: "temperature", value: data.climateState.outsideTemp, unit: "F")
+    sendEvent(name: "swVersion", value: data.version)
+    
+    if (data.availableVersion) {
+    	sendEvent(name: "updateAvailable", value: data.availableVersion)
+    } else {
+    	sendEvent(name: "updateAvailable", value: "none")
+    }
+
+    // Dispatch to each child
+	childDevices.each { it.processData(data) }
+}
+
+def doRefresh() {
+    log.debug "Refreshing car data now; last update=${device.getLastActivity()}"
+    def data = parent.refresh(this)
+    processData(data)
+
+    if (data?.car_state == "Driving") {
+        log.debug "Refreshing more often because Driving"
+        runEvery1Minute(refreshWhileDriving)
+    }
+}
+
+// Only use this for the manual refresh tile action
+def refresh() {
+    log.debug "Triggering refresh by command"
+    doRefresh()
+}
+
+def refreshWhileDriving() {
+    log.debug "Executing 'refreshWhileDriving'"
+    def data = parent.refresh(this)
+    processData(data)
+
+    if (data?.car_state != "Driving") {
+        unschedule(refreshWhileDriving)
+    }
+}
+
+def beep() {
+    log.debug "Executing 'beep'"
+    def data = parent.beep(this)
+}
+
+def wake() {
+    log.debug "Executing 'wake'"
+    def result = parent.wake(this)
+    if (result) doRefresh()
+}
+
+def lock() {
+    log.debug "Executing 'lock'"
+    def result = parent.lock(this)
+    if (result) doRefresh()
+}
+
+def unlock() {
+    log.debug "Executing 'unlock'"
+    def result = parent.unlock(this)
+    if (result) doRefresh()
+}
