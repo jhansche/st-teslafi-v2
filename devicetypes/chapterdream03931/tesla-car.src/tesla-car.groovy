35a89fe5904daa88e486663124496042c8e9792e
Joe Hansche <madcoder@gmail.com>
2021-02-22 17:26:13 -0500

    Presence, ETA, and formatting

--- devicetypes/chapterdream03931/tesla-car.src/tesla-car.groovy
+++ devicetypes/chapterdream03931/tesla-car.src/tesla-car.groovy
@@ -1,4 +1,3 @@
-
 /**
  *  Tesla
  *
@@ -29,6 +28,12 @@ metadata {
         capability "chapterdream03931.driveState"
         capability "chapterdream03931.vehicleModelInfo"
         capability "chapterdream03931.tmpGeolocation"
+        capability "Presence Sensor"
+        capability "Estimated Time of Arrival"
+
+        // TODO: move to capability
+        attribute "distanceToHome", "string"
+        attribute "timeToHome", "string"
     }
 }
 
@@ -46,26 +51,33 @@ def processData(data) {
     }
 
     if (data.vin?.substring(9, 10) == "J") {
-    	sendEvent(name: "year", value: 2018)
+        sendEvent(name: "year", value: 2018)
         // TODO: other VIN attributes
     }
-	if (data.optionCodes?.size() > 0) {
+    if (data.optionCodes?.size() > 0) {
         if (data.optionCodes.contains("MDL3")) {
-        	sendEvent(name: "make", value: "Tesla")
+            sendEvent(name: "make", value: "Tesla")
             sendEvent(name: "model", value: "Model 3")
-            
+
             if (data.optionCodes.contains("BT37") && data.optionCodes.contains("DV4W")) {
-	            sendEvent(name: "trim", value: "LR AWD")
+                sendEvent(name: "trim", value: "LR AWD")
                 // TODO: other trim codes (RWD, MR, SR, etc)
                 //  https://tesla-api.timdorr.com/vehicle/optioncodes
+                // FIXME: these option codes aren't valid anymore
             }
         }
     }
     sendEvent(name: "odometer", value: data.vehicleState.odometer, unit: 'mi')
     sendEvent(name: "range", value: data.chargeState.batteryRange, unit: 'mi')
-    
+
     sendEvent(name: "heading", value: data.driveState.heading)
-    
+    sendEvent(name: "latitude", value: data.driveState.latitude)
+    sendEvent(name: "longitude", value: data.driveState.longitude)
+
+    if (data.vehicleState) {
+        sendEvent(name: "presence", value: data.vehicleState.presence)
+    }
+
     if (data.car_state == "Driving") {
         sendEvent(name: "driveState", value: "drive")
         sendEvent(name: "speed", value: data.driveState.speed, unit: "mph")
@@ -74,4 +86,23 @@ def processData(data) {
         sendEvent(name: "speed", value: 0, unit: "mph")
     }
 
+    if (device.currentValue("presence") == "not present") { // JHH not present
+        def etaData = parent.parent.findEtaHome(data.driveState.latitude, data.driveState.longitude)
+        log.debug("JHH eta response $etaData")
+        // Estimated Time Of Arrival
+        if (etaData?.result == true) {
+            // TODO: switch to eta-home capability
+            sendEvent(name: "distanceToHome", value: etaData.distance.text)
+            sendEvent(name: "timeToHome", value: etaData.time.text)
+            def eta = Calendar.getInstance(location.timeZone)
+            eta.add(Calendar.SECOND, etaData.time.value)
+            // TODO: switch to a better eta-home capability, with both distance and time
+            sendEvent(name: "eta", value: eta.time.format("yyyy-MM-dd'T'HH:mm:ss", location.timeZone))
+        } else {
+            log.debug("ETA failed: ${etaData}")
+        }
+    } else {
+        // Clear the ETA if we're already home
+        sendEvent(name: "eta", value: null)
+    }
 }
